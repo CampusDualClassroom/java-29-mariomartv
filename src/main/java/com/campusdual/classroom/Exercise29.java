9993948dd3af0934d69aab539ea020d4652af686
Mario <marionuman@gmail.com>
2024-09-30 10:22:18 +0200

    Ejercicio 29 Completado

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -2,6 +2,10 @@ package com.campusdual.classroom;
 
 public class Exercise29 {
     public static void main(String[] args) {
-
+    try{
+    throw new Exception("Excepcion lanzada por Mario");
+    } catch (Exception e) {
+        e.getMessage();
+    }
     }
 }
